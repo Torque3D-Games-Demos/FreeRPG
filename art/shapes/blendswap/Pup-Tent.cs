cd879b079de336f8a032eda6a53083b133d9fa8b
Michael Haslam <hreikin@gmail.com>
2015-07-31 00:35:22 +0100

    level with this commit from FreeRPG-Torque3D repo 1aa8589144da305acb4b76558a424baf677d80cb

---
+++ art/shapes/blendswap/Pup-Tent.cs
@@ -0,0 +1,24 @@
+
+singleton TSShapeConstructor(Pup-Tent)
+{
+   baseShape = "./Pup-Tent.dae";
+   adjustCenter = "1";
+   adjustFloor = "1";
+   loadLights = "0";
+};
+
+singleton TSShapeConstructor(Pup-Tent)
+{
+   baseShape = "./Pup-Tent.dae";
+   adjustCenter = "1";
+   adjustFloor = "1";
+   loadLights = "0";
+};
+
+singleton TSShapeConstructor(0)
+{
+   baseShape = "./Pup-Tent.dae";
+   adjustCenter = "1";
+   adjustFloor = "1";
+   loadLights = "0";
+};
